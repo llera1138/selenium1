298501e4a22298bcd426a1fe340c60af26de9062
LERA <llera1138@yandex.ru>
2024-05-05 12:58:05 +0300

    change

--- src/test/java/AppOrderNegativeTest.java
+++ src/test/java/AppOrderNegativeTest.java
@@ -5,28 +5,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
-public class TestUiValid {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppOrderpositiveTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
+    public static void setupAll() {
         WebDriverManager.chromedriver().setup();
-
     }
 
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999/");
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -35,15 +38,71 @@ public class TestUiValid {
         driver = null;
     }
 
+    @Test
+    void shouldTestV1() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Хвиюзова Валерия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualTextElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualTextElement.isDisplayed());
+    }
 
     @Test
-    public void testValid1() {
-        driver.findElement(By.cssSelector("[data-test-id ='name'] input")).sendKeys("Валерия Хвиюзова");
-        driver.findElement(By.cssSelector("[data-test-id ='phone'] input")).sendKeys("+79917893394");
-        driver.findElement(By.cssSelector("[data-test-id ='agreement']")).click();
-        driver.findElement(By.className("button")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id = order-success]")).getText().trim();
-        assertEquals(expected, actual);
+    void shouldTestNegative() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).isDisplayed());
+    }
+
+    @Test
+    void shouldTestNegativeName() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Luppa Mappuppa");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).isDisplayed());
+    }
+
+    @Test
+    void shouldTestNegativePhone() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Хвиюзова Валерия");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).isDisplayed());
+    }
+
+    @Test
+    void shouldTestNegativePhoneIncorrect() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Хвиюзова Валерия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Вася");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).isDisplayed());
+    }
+
+    @Test
+    void shouldTestNegativeСheckbox() throws InterruptedException {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Хвиюзова Валерия");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
     }
 }
